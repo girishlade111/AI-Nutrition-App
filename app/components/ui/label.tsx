87cdfb218c280dbe8d0e50eeea03a4b31f3ef9a1
Girish Lade <girishlade111@gmail.com>
2026-04-27 03:05:42 -0700

    Add Input and Label UI components

--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -9,7 +9,10 @@ export const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
     ({ className, children, ...props }, ref) => (
         <label
             ref={ref}
-            className={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
+            className={cn(
+                "text-sm font-medium text-slate-700 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+                className
+            )}
             {...props}
         >
             {children}
